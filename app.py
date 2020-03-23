392dbb3d15795e5e994ce106809d158bf09402e7
liupeng <liupeng.0@outlook.com>
2020-03-23 16:28:11 +0800

    route V 1.0.1

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
+# import package
 from flask import Flask
 
 app = Flask(__name__)
 
-
+# tell Flask 触发 function url
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
+@app.route('/index')
+def index():
+    return "Index Page"
+
+@app.route('/user/<name>')
+def paly(name):
+    return "hello %s" % name
 
 if __name__ == '__main__':
     app.run()
