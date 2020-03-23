f2b8ddbed6f3628965c78d9f6fc79da0593daefe
liupeng <liupeng.0@outlook.com>
2020-03-23 21:53:46 +0800

    bluePrint templates 渲染V 1.0.8

--- application/good.py
+++ application/good.py
@@ -1,7 +1,12 @@
 # good.py
-from flask import Blueprint
+from flask import Blueprint, render_template
 
-good_blueprint = Blueprint('good', __name__, url_prefix='/api/good')
+good_blueprint = Blueprint(
+    'good',
+    __name__,
+    url_prefix='/api/good',
+    template_folder='application_templates'
+)
 
 @good_blueprint.route('/')
 def getGoods():
@@ -10,3 +15,7 @@ def getGoods():
 @good_blueprint.route('/<int:catagory_id>')
 def getGoodsInCatagory(catagory_id):
     return "good-2 %d" % catagory_id
+
+@good_blueprint.route('/index/<string:name>')
+def index(name):
+    return render_template('index.html', name=name)
