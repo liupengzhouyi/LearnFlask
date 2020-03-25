7e220e30164b3dc56f836a615452c5cd33679a25
liupeng <liupeng.0@outlook.com>
2020-03-25 15:06:50 +0800

    add todo in database V 1.0.17

---
+++ models/todo.py
@@ -0,0 +1,51 @@
+# models.py
+
+from db import db
+from sqlalchemy import and_
+
+class Todo(db.Model):
+
+    # （设置表名）
+    __tablename__ = 'todo_list'
+
+    # （设置主键）
+    id = db.Column(db.Integer, primary_key=True,)
+
+    do = db.Column(db.String(255),)
+
+    year = db.Column(db.String(10),)
+
+    month = db.Column(db.String(10),)
+
+    day = db.Column(db.String(10),)
+
+    done = db.Column(db.String(10),)
+
+    # 返回一个可以用来表示对象的可打印字符串：（相当于java的toString）
+    def __repr__(self):
+        return '<做什么：%r 年：%r 月：%r 日： %r 是否做完： %r>' % (self.do, self.year, self.month, self.day, self.done)
+
+# 操作数据库
+
+# 增
+def add_object(todo):
+    db.session.add(todo)
+    db.session.commit()
+    print("添加 % r 完成" % todo.__repr__)
+
+# 查 （用到and的时候需要导入库from sqlalchemy import and_）
+def query_object(user, query_condition_u, query_condition_p):
+    result = user.query.filter(and_(user.username == query_condition_u, user.password == query_condition_p))
+    print("查询 % r 完成" % user.__repr__)
+    return result
+
+# 删
+def delete_object(todo):
+    result = todo.query.filter(todo.username == '11111').all()
+    db.session.delete(result)
+    db.session.commit()
+
+#改
+def update_object(todo):
+    result = todo.query.filter(todo.username == '111111').all()
+    result.title = 'success2018'
--- todo/todo.py
+++ todo/todo.py
@@ -1,5 +1,7 @@
 # login.py
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
+
+from models.todo import Todo, add_object
 
 todo_blueprint = Blueprint(
     'todo',
@@ -10,4 +12,26 @@ todo_blueprint = Blueprint(
 
 @todo_blueprint.route('/')
 def index():
+    return render_template('todoIndex.html')
+
+@todo_blueprint.route('/add')
+def add():
+    return render_template('addtodo.html')
+
+
+@todo_blueprint.route('/getDate', methods=['POST', 'GET'])
+def getDate():
+    todoname = request.form.get('todoname')
+    year = request.form.get('year')
+    month = request.form.get('month')
+    day = request.form.get('day')
+
+    todo = Todo()
+    todo.done = '0'
+    todo.do = todoname
+    todo.year = year
+    todo.month = month
+    todo.day = day
+    print("添加 % r 完成" % todo.__repr__)
+    add_object(todo)
     return render_template('todoIndex.html')
